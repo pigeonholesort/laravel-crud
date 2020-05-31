b1c51a6d97eb0be10ec7f0fac84af90c73eef625
pige <akatsukisatoshi@gmail.com>
2020-05-31 16:38:06 +0900

    エラーページの構築

--- 5.7/app/Http/Kernel.php
+++ 5.7/app/Http/Kernel.php
@@ -21,6 +21,12 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        // $middlewareGroups['web'] から移動
+        // 参照: https://laracasts.com/discuss/channels/laravel/getting-authuser-on-custom-404-page/replies/293136
+        \App\Http\Middleware\EncryptCookies::class,
+        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        \Illuminate\Session\Middleware\StartSession::class,
+        \App\Http\Middleware\CheckLocale::class, // 独自ミドルウェア
     ];
 
     /**
@@ -30,14 +36,10 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
-            \App\Http\Middleware\EncryptCookies::class,
-            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\CheckLocale::class,
         ],
 
         'api' => [
